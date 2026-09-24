742b85015ebb2923c8bb9456675b43e925ca5ff8
Jeevitha Jagadeesh <jeevithajagadeesh33@gmail.com>
2026-09-24 22:55:48 +0530

    push

---
+++ src/26_Remove_duplicates.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0;
+
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+
+        return j + 1;
+    }
+};
+
+int main() {
+
+    vector<int> nums = {1, 1, 2, 2, 3, 3, 4};
+
+    Solution obj;
+
+    int k = obj.removeDuplicates(nums);
+
+    cout << "Number of unique elements: " << k << endl;
+
+    cout << "Array after removing duplicates: ";
+
+    for(int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
